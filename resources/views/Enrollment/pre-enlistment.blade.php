39748f631fed98fbde2c036fa7f4ebf76b552b29
aacast <alyannacastillon@gmail.com>
2021-08-10 22:39:30 +0800

    Aly's Push :)

--- resources/views/Enrollment/pre-enlistment.blade.php
+++ resources/views/Enrollment/pre-enlistment.blade.php
@@ -7,56 +7,95 @@
     <div class="mx-auto d-block col-sm-8" style="margin:3%" >
         <form class="row g-3">
             <div class="col-md-4">
-                <label for="inputLastName" class="form-label">Last Name</label>
-                <input type="text" class="form-control" id="inputLastName">
+                <label for="exampleFormControlInput1" class="form-label">Last Name</label>
+                <input type="text" class="form-control" name="studLastNname" >
             </div>
             <div class="col-md-4">
-                <label for="inputFirstName" class="form-label">First Name</label>
-                <input type="text" class="form-control" id="inputFirstName">
+                <label for="exampleFormControlInput1" class="form-label">First Name</label>
+                <input type="text" class="form-control" name="studFirstName">
             </div>
             <div class="col-md-2">
-                <label for="inputMiddleName" class="form-label">Middle Name</label>
-                <input type="text" class="form-control" id="inputMiddleName">
+                <label for="exampleFormControlInput1" class="form-label">Middle Name</label>
+                <input type="text" class="form-control" name="studMiddleName">
             </div>
             <div class="col-md-2">
-                <label for="inputSuffix" class="form-label">Suffix</label>
-                <input type="text" class="form-control" id="inputSuffix">
+                <label for="exampleFormControlInput1" class="form-label">Suffix</label>
+                <input type="text" class="form-control" name="studSuffix">
             </div>
             <div class="col-md-6">
-                <label for="inputEmail4" class="form-label">Email</label>
-                <input type="email" class="form-control" id="inputEmail4">
+                <label for="exampleFormControlInput1" class="form-label">Landline</label>
+                <input type="text" class="form-control" name="studLandline">
             </div>
             <div class="col-md-6">
-                <label for="inputPassword4" class="form-label">Password</label>
-                <input type="password" class="form-control" id="inputPassword4">
+            <label for="exampleFormControlInput1" class="form-label">Mobile Number</label>
+                <input type="text" class="form-control" name="studMobileNumber">
             </div>
             <div class="col-12">
-                <label for="inputAddress" class="form-label">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            <label for="exampleFormControlInput1" class="form-label">Address</label>
+                <input type="text" class="form-control" name="studAddress">
             </div>
-            <div class="col-12">
-                <label for="inputAddress2" class="form-label">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            <div class="col-6">
+            <label for="exampleFormControlInput1" class="form-label">Birth Date</label>
+                <input type="date" class="form-control" name="studBirthDate">
             </div>
             <div class="col-md-6">
-                <label for="inputCity" class="form-label">City</label>
-                <input type="text" class="form-control" id="inputCity">
+            <label for="exampleFormControlInput1" class="form-label">Age</label>
+                <input type="text" class="form-control" name="studAge">
+            </div>
+            <div class="col-md-6">
+            <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <input type="email" class="form-control" name="studAge">
+            </div>
+            <div class="col-md-6">
+            <label for="exampleFormControlInput1" class="form-label">Password</label>
+                <input type="password" class="form-control" name="studPass">
+            </div>
+            <div class="col-md-6">
+            <label for="studSHS" class="form-label">Senior High School</label>
+                <input type="text" class="form-control" name="studSHS">
             </div>
             <div class="col-md-4">
-                <label for="inputState" class="form-label">State</label>
-                <select id="inputState" class="form-select">
+                <label for="studYearGrad" class="form-label"> Year Graduated</label>
+                <select id="studYearGrad" class="form-select">
                 <option selected>Choose...</option>
                 <option>...</option>
                 </select>
             </div>
-            <div class="col-md-2">
-                <label for="inputZip" class="form-label">Zip</label>
-                <input type="text" class="form-control" id="inputZip">
+            <div class="col-md-4">
+                <label for="studPrefCourse1" class="form-label">Preferred Course 1</label>
+                <select id="studPrefCourse1" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+                </select>
             </div>
+            <div class="col-md-4">
+                <label for="studPrefCourse2" class="form-label">Preferred Course 2</label>
+                <select id="studPrefCourse2" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+                </select>
+            </div>
+            <div class="col-md-4">
+                <label for="studPrefCourse3" class="form-label">Preferred Course 3</label>
+                <select id="studPrefCourse4" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+                </select>
+            </div>
+
             <div class="col-12">
-                <button type="submit" class="btn btn-dark">Submit</button>
+            <button type="button" onclick ="alert('Data Submitted!')" class="btn btn-dark">Submit</button>
+            </div>
+
+            <div class="col-12">
+            <a href="/SEU/public/status"><button type="button" class="btn btn-custom">Find Status</button></a>
             </div>
         </form>
     </div>
     
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+   
 @endsection

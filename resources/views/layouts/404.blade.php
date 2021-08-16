445dd120eb132d7f17642a0cec282f154b3ab69f
janna <rijanuy88@gmail.com>
2021-08-16 10:33:33 +0800

    ADDED: 404 not found

---
+++ resources/views/layouts/404.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 Not Found!</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
+
+    <style>
+        .container{
+            padding-top: 5%;
+        }
+        .page_404{
+            padding: 40px 0;
+            background: #fff;
+            font-family: "Arvo", serif;
+        }
+        .page_404 img{
+            width:100%;
+        }
+        .four_zero_four_bg{
+            background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+            height: 400px;
+            background-position: center;
+        }
+        .four_zero_four_bg h1{
+            font-size: 80px;
+        }
+        .four_zero_four_bg h3{
+            font-size: 80px;
+        }
+        .link_404{
+            color: #fff !important;
+            padding: 10px 20px;
+            background: #39ac31;
+            margin: 20px 0;
+            display: inline-block;
+            text-decoration: none;
+        }
+        .contant_box_404{
+            margin-top: -50px
+        }
+
+    </style>
+</head>
+
+<body>
+    <section>
+        <div class="container ">
+            <div class="row">
+                <div class="col-sm-12 d-flex justify-content-center ">
+                    <div class="col-sm-10 col-sm-offset-1 text-center">
+                        <div class="four_zero_four_bg ">
+                            <h1 class="text-center">404</h1>
+                        </div>
+                        <div class="contant_box_404">
+                            <h3 class="h2">
+                                Looks like you're lost
+                            </h3>
+                            <p>the page you are looking for is not available!</p>
+                            <a href="" class="link_404">Go to Home</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing');
 });
+Route::get('/404', function () {
+    return view('layouts.404');
+});
 
 // '/document' name you want
 //  'Enrollment.documents' location

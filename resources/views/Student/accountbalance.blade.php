f07b83c78ad97aead6e45d8daffa7e16c527014a
janna <rijanuy88@gmail.com>
2021-08-16 09:38:01 +0800

    ADDED: account balance, minor fix sidebar&nav

--- resources/views/Student/accountbalance.blade.php
+++ resources/views/Student/accountbalance.blade.php
@@ -1,2 +1,231 @@
-@extends('layouts.Studtabs')
-{{-- @extends('layouts.stud') --}}
+{{-- @extends('layouts.Studtabs') --}}
+@extends('layouts.stud')
+
+@section('title', 'stud-balance')
+
+<style>
+    .card-body{
+        display: flex;
+    }
+
+    span{
+        margin-right: 5%;
+    }
+
+    ul{
+        list-style-type: none;
+    }
+</style>
+
+@section('content')
+<div class="card w-75 mx-auto">
+    <div class="card-header text-white" style="background-color: #e19021">
+        MY ACCOUNT BALANCE
+    </div>
+    <div div class="card-body">
+        <img src="assets/icon.png" alt="" width="100" height="100" class="rounded-circle me-2">
+        <span>  
+            <ul>
+                <li style="color:gray">STUDENT NAME</li>
+                <li><h6>Sy, Angela Rizza Lorraine G. III</h6></li>
+                <li style="color:gray">STUDENT ID</li>
+                <li><h6>12345678</h6></li>
+            </ul>
+        </span>
+        <span>
+            <ul>
+                <li style="color:gray">COURSE</li>
+                <li><h6>BS Computer Science</h6></li>
+                <li style="color:gray">SECTION</li>
+                <li><h6>ComSci 3-A</h6></li>
+            </ul>
+        </span>
+        <span>
+            <ul>
+                <li style="color:gray">ACADEMIC PERIOD</li>
+                <li><h6>A.Y. 2021-2022, 1st Semester</h6></li>
+                <li style="color:gray">STATUS</li>
+                <li><h6>Enrolled</h6></li>
+            </ul>
+        </span>
+    </div>
+    {{-- contents --}}
+        {{-- BALANCE --}}
+        <div class="container " style="margin-Top:2%">
+            <div class="row" style="color:#6a1717">
+                <div class="col-8">
+                    <h2>BALANCE:</h2>
+                </div>
+                <div class="col-4" >
+                    <h2>Total: 1,000,000.00</h2>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="container heading">
+                <div class="row">
+                    <div class="col-8">
+                        <h5><strong>Description</strong></h5>
+                    </div>
+                    <div class="col-4">
+                        <h5><strong>Amount</strong></h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container tuition">
+                <div class="row">
+                        <div class="col-8">
+                                <h5><strong>Tuition</strong></h5>
+                        </div>
+                        <div class="col-4">
+                            <h6>1500</h6>
+                        </div>
+                </div>
+                {{-- sub contents per main fee --}}
+                <div class="col">
+                    {{-- for loop data --}}
+                    <div class="row">
+                        <div class="col-6">
+                            <h5>&nbsp Lab fees</h5>
+                        </div>
+                        <div class="col-2">
+                            <h6>1000</h6>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <h5>&nbsp Lab fees</h5>
+                        </div>
+                        <div class="col-2">
+                            <h6>1000</h6>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <h5>&nbsp Lab fees</h5>
+                        </div>
+                        <div class="col-2">
+                            <h6>500</h6>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
+            <hr>
+
+            <div class="container misc">
+                <div class="row">
+                    <div class="col-8">
+                        <h5><strong>Miscellaneous Fees:</strong></h5>
+                    </div>
+                    <div class="col-4">
+                        <h6>15000</h6>
+                    </div>
+                </div>
+
+                <div class="col">
+                    {{-- for loop data --}}
+                        <div class="row">
+                            <div class="col-6">
+                                <h5>&nbsp Lab fees</h5>
+                            </div>
+                            <div class="col-2">
+                                <h6>100</h6>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+                                <h5>&nbsp Lab fees</h5>
+                            </div>
+                            <div class="col-2">
+                                <h6>100</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+            {{-- PAYMENTS --}}
+            <div class="container" style="margin-Top:2%">
+                <div class="row" style="color:green">
+                    <div class="col-8">
+                        <h2>PAYMENTS & CREDITS:</h2>
+                    </div>
+                    <div class="col-4">
+                        <h2>Total: 1,000.00</h2>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="container heading">
+                    <div class="row">
+                        <div class="col-6">
+                            <h5><strong>Description</strong></h5>
+                        </div>
+                        <div class="col-2">
+                            <h5><strong>Due</strong></h5>
+                        </div>
+                        <div class="col-2">
+                            <h5><strong>Payments</strong></h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container tuition">
+                    <div class="row">
+                        <div class="col-6">
+                            <h5><strong>Prelim</strong></h5>
+                        </div>
+                        <div class="col-2">
+                            <h6>1500</h6>
+                        </div>
+                        <div class="col-2">
+                            <h6>1500</h6>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <h5><strong>Midterms</strong></h5>
+                        </div>
+                        <div class="col-2">
+                            <h6>1500</h6>
+                        </div>
+                        <div class="col-2">
+                            <h6>1500</h6>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <h5><strong>Finals</strong></h5>
+                        </div>
+                        <div class="col-2">
+                            <h6>1500</h6>
+                        </div>
+                        <div class="col-2">
+                            <h6>1500</h6>
+                        </div>
+                    </div>
+
+                </div>
+                
+                <hr>
+
+                
+            </div>
+
+
+            
+        </div>
+</div>
+@endsection
+
--- resources/views/layouts/stud.blade.php
+++ resources/views/layouts/stud.blade.php
@@ -19,44 +19,36 @@
         background-color: #6a1717;
     }
     
-    .list-group-item{
-        background-color: #6a1717;
-        color:white;
+    body {
+        background-color: #fbfbfb;
     }
-
     @media (min-width: 991.98px) {
         main {
             padding-left: 240px;
+            padding-top: 2%;
         }
     }
-
     /* Sidebar */
     .sidebar {
         position: fixed;
         top: 0;
         bottom: 0;
         left: 0;
+        padding: 58px 0 0;
         /* Height of navbar */
         box-shadow: 0 2px 5px 0 rgb(0 0 0 / 5%), 0 2px 10px 0 rgb(0 0 0 / 5%);
         width: 240px;
         z-index: 600;
     }
-
-    a{
-        text-decoration:none;
-    }
-
     @media (max-width: 991.98px) {
         .sidebar {
             width: 100%;
         }
     }
-
     .sidebar .active {
         border-radius: 5px;
         box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
     }
-
     .sidebar-sticky {
         position: relative;
         top: 0;
@@ -66,44 +58,64 @@
         overflow-y: auto;
         /* Scrollable contents if viewport is shorter than content. */
     }    
-     .custom-toggler .navbar-toggler-icon {
+    .custom-toggler .navbar-toggler-icon {
         background-image: url(
         "data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+    }
+
+    a{
+        text-decoration:none;
+    }
+    #sidebarMenu{
+        background-color: #6a1717;
+        color:white;
+    }
+    .list-group-item{
+        background-color: #6a1717;
+        color:white;
+    }
+    @media (max-width: 991.98px) {
+        .sidebar {
+            width: 100%;
         }
+    }
 </style>
 <!--Main Navigation-->
 <header>
     <!-- Sidebar -->
-    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse navbar-custom">
+    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse">
         <div class="position-sticky">
             <div class="list-group list-group-flush mx-3 mt-4">
-                <a href="/SEU/public/dashboard">
-                    <img src="assets/SEU_Mini Logo.png" alt="" width="200" height="55">
-                </a>
-                <hr>
-                <ul id="collapseExample1" class="collapse show list-group list-group-flush">
-                   
-                    <li class="list-group-item py-1">
-                        <a href="" class="text-reset">Home</a>
-                    </li>
-                    <li class="list-group-item py-1">
-                        <a href="" class="text-reset">My Registration</a>
-                    </li>
-                    <li class="list-group-item py-1">
-                        <a href="" class="text-reset">My Grades</a>
-                    </li>
-                    <li class="list-group-item py-1">
-                        <a href="/SEU/public/account-balance" class="text-reset">My Account Balance</a>
-                    </li>
-                    <li class="list-group-item py-1">
-                        <a href="/SEU/public/curriculum" class="text-reset">My Curriculum</a>
-                    </li>
-                    <li class="list-group-item py-1">
-                        <a href="" class="text-reset">Class Offerings</a>
-                    </li>
-                    <li class="list-group-item py-1">
-                        <a href="/SEU/public/" class="text-reset">Log out</a>
-                    </li>
+                {{-- <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                    <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                    <span class="fs-4"><strong>mdo</strong></span>
+                </a> --}}
+                {{-- <hr> --}}
+                <ul
+                    id="collapseExample1"
+                    class="collapse show list-group list-group-flush"
+                    >
+                <li class="list-group-item py-1">
+                    <a href="" class="text-reset">Home</a>
+                </li>
+                <li class="list-group-item py-1">
+                    <a href="" class="text-reset">My Registration</a>
+                </li>
+                <li class="list-group-item py-1">
+                    <a href="" class="text-reset">My Grades</a>
+                </li>
+                <li class="list-group-item py-1">
+                    <a href="/SEU/public/account-balance" class="text-reset">My Account Balance</a>
+                </li>
+                <li class="list-group-item py-1">
+                    <a href="/SEU/public/curriculum" class="text-reset">My Curriculum</a>
+                </li>
+                <li class="list-group-item py-1">
+                    <a href="" class="text-reset">Class Offerings</a>
+                </li>
+                <li class="list-group-item py-1">
+                    <a href="/SEU/public/" class="text-reset">Log out</a>
+                </li>
                 </ul>
             </div>
         </div>
@@ -111,13 +123,18 @@
     <!-- Sidebar -->
 
     <!-- Navbar -->
-     <!-- Navbar -->
-     <nav id="main-navbar" class="navbar navbar-expand-lg fixed-top">
+    <nav id="main-navbar" class="navbar navbar-expand-lg fixed-top navbar-custom">
         <!-- Container wrapper -->
         <div class="container-fluid">
         <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
+        <!-- Brand -->
+        <a class="navbar-brand" href="/SEU/public/dashboard">
+            <img src="assets/SEU_Mini Logo.png" height="50"
+            alt=""
+            loading="lazy">
             
         </a>
         {{-- <!-- Search form -->
